d9dfbc5d83d5e0e241d1f60b4c248cd45669779e
RobertSouDev <150874820+RobertSouDev@users.noreply.github.com>
2024-02-19 22:34:47 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+        // Pessoa -> Tipo do Objeto (Classer)
+        // P -> Objeto da classe pessoa
+        // new Pessoa() -> espaço de memoria que reservado para o objeto p
+        System.out.println("teste");
+    }
+}
