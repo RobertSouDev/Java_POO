55e09279ef4992a27cd5326230254e6ac5c21175
RobertSouDev <rogerhallks1@gmail.com>
2024-02-19 23:08:20 -0300

    Modificador final

---
+++ src/conceito_Heranca/Categorias.java
@@ -0,0 +1,15 @@
+package conceito_Heranca;
+
+public class Categorias {
+    private Categorias(){
+
+    }
+
+    public static final int Junior= 1;
+
+    public static final int Pleno= 4;
+
+    public static final int Senior= 7;
+
+    public static final int Expecialista= 10;
+}
